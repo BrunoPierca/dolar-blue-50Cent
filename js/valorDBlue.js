99de94e69b112f369e5a3b9b3216884bf0e9c7b6
BrunoPierca <pierca18@gmail.com>
2021-08-31 05:16:43 -0300

    Corregí la funcion numeroRandom y agregue funcionalidades al chart

--- js/valorDBlue.js
+++ js/valorDBlue.js
@@ -1,159 +1,293 @@
-const fiftyh1 = document.getElementById('fiftyh1')
-const fiftyFloat = document.getElementById('fiftyFloat')
-const pcomprablue = document.getElementById('comprablue')
-const pventablue = document.getElementById('ventablue')
+const fiftyh1 = document.getElementById("fiftyh1");
+const fiftyFloat = document.getElementById("fiftyFloat");
+const pcomprablue = document.getElementById("comprablue");
+const pventablue = document.getElementById("ventablue");
 
 async function getDBlue() {
-    const bluelyticsAPI = await fetch('https://api.bluelytics.com.ar/v2/latest');
-    const data = await bluelyticsAPI.json()
-    const dblue = await data.blue;
-    const dblueVenta = await dblue.value_sell;
-    const decimaldblueVenta = (dblueVenta/2 - Math.trunc(dblueVenta/2))*100
-    fiftyh1.innerHTML = Math.trunc(dblueVenta/2);
-    fiftyFloat.innerHTML = '.' + decimaldblueVenta.toFixed(0)
-    const dblueCompra = await dblue.value_buy;
-    pcomprablue.innerHTML = 'Compra: ' + dblueCompra + '$'
-    pventablue.innerHTML = 'Venta: ' +'<span>' +  dblueVenta + '</span>' + '$'
+  const bluelyticsAPI = await fetch("https://api.bluelytics.com.ar/v2/latest");
+  const data = await bluelyticsAPI.json();
+  const dblue = await data.blue;
+  const dblueVenta = await dblue.value_sell;
+  const decimaldblueVenta = (dblueVenta / 2 - Math.trunc(dblueVenta / 2)) * 100;
+  fiftyh1.innerHTML = Math.trunc(dblueVenta / 2);
 
+  const float = decimaldblueVenta.toFixed(0);
+  if (float == 0) {
+    fiftyFloat.style.display = "none";
+  }
+  fiftyFloat.innerHTML = "." + float;
+  const dblueCompra = await dblue.value_buy;
+  pcomprablue.innerHTML = "Compra: $" + dblueCompra;
+  pventablue.innerHTML = "Venta: " + "<strong>$" + dblueVenta + "</strong>";
 }
 
-getDBlue()
-    
-    // Cancion Random con link
+getDBlue();
 
-    const generateRandomNumber = (min, max) =>  {
-        return Math.floor(Math.random() * (max - min) + min);
-          };
-    const cancion = document.getElementById('cancion')
-    
+const fecha = new Date();
+const mes = fecha.getMonth() + 1;
+const anio = fecha.getFullYear();
 
-    //  array de objetos 
-    let arrayCanciones = [ ]; 
+// Cancion Random con link
 
-    let numRandom = generateRandomNumber(0, arrayCanciones.length)
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+const cancion = document.getElementById("cancion");
+const album = document.getElementById("album");
 
-    
-    function AgregarCancion(nombre , link , disco) {
-        let objCancion = {cancion : nombre, linkyt: link , album: disco};
-        arrayCanciones.push(objCancion);
-    }
+//  array de objetos
+let arrayCanciones = [];
 
-    AgregarCancion('«Poor Lil Rich»' , 'https://www.youtube.com/watch?v=_x1vRIfotcw' , 'Get rich or die tryin´')
-    AgregarCancion('«Like My Style»' , 'https://www.youtube.com/watch?v=o3Gm41HKdQA' , 'Get rich or die tryin´')
-    AgregarCancion('«In Da Club»' , 'https://www.youtube.com/watch?v=5qm8PH4xAss' , 'Get rich or die tryin´')
-    AgregarCancion('«21 Questions»' , 'https://www.youtube.com/watch?v=cDMhlvbOFaM' , 'Get rich or die tryin´')
-    AgregarCancion('«P.I.M.P.»' , 'https://www.youtube.com/watch?v=Jy1D6caG8nU' , 'Get rich or die tryin´')
-    AgregarCancion('«Many Men (Wish Death)»' , 'https://www.youtube.com/watch?v=5D3crqpClPY' , 'Get rich or die tryin´')
-    AgregarCancion('«Patiently Waiting»' , 'https://www.youtube.com/watch?v=GN3ypSLNIDA' , 'Get rich or die tryin´')
-    AgregarCancion('«What Up Gangsta»' , 'https://www.youtube.com/watch?v=xIl4ZGd8MPc' , 'Get rich or die tryin´')
-    AgregarCancion('«Heat»' , 'https://www.youtube.com/watch?v=LZIc8VSonVY' , 'Get rich or die tryin´')
-    AgregarCancion('«If I Can´t»' , 'https://www.youtube.com/watch?v=z5zz7GHU4ec' , 'Get rich or die tryin´')
-    AgregarCancion('«Don´t Push Me»' , 'https://www.youtube.com/watch?v=Mj4YJHDpamg' , 'Get rich or die tryin´')
-    AgregarCancion('«Gotta Make It to Heaven»' , 'https://www.youtube.com/watch?v=_NNfJfa672I' , 'Get rich or die tryin´')
-    AgregarCancion('«High All the Time»' , 'https://www.youtube.com/watch?v=m13_w0h6s4M' , 'Get rich or die tryin´')
-    AgregarCancion('«Blood Hound»' , 'https://www.youtube.com/watch?v=5NRy-Xv2yQI' , 'Get rich or die tryin´')
-    AgregarCancion('«Back Down»' , 'https://www.youtube.com/watch?v=3Opytt7bblU' , 'Get rich or die tryin´')
+function AgregarCancion(nombre, link, disco) {
+  let objCancion = { cancion: nombre, linkyt: link, album: disco };
+  arrayCanciones.push(objCancion);
+}
 
+AgregarCancion(
+  "«Poor Lil Rich»",
+  "https://www.youtube.com/watch?v=_x1vRIfotcw",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Like My Style»",
+  "https://www.youtube.com/watch?v=o3Gm41HKdQA",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«In Da Club»",
+  "https://www.youtube.com/watch?v=5qm8PH4xAss",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«21 Questions»",
+  "https://www.youtube.com/watch?v=cDMhlvbOFaM",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«P.I.M.P.»",
+  "https://www.youtube.com/watch?v=Jy1D6caG8nU",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Many Men (Wish Death)»",
+  "https://www.youtube.com/watch?v=5D3crqpClPY",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Patiently Waiting»",
+  "https://www.youtube.com/watch?v=GN3ypSLNIDA",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«What Up Gangsta»",
+  "https://www.youtube.com/watch?v=xIl4ZGd8MPc",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Heat»",
+  "https://www.youtube.com/watch?v=LZIc8VSonVY",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«If I Can´t»",
+  "https://www.youtube.com/watch?v=z5zz7GHU4ec",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Don´t Push Me»",
+  "https://www.youtube.com/watch?v=Mj4YJHDpamg",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Gotta Make It to Heaven»",
+  "https://www.youtube.com/watch?v=_NNfJfa672I",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«High All the Time»",
+  "https://www.youtube.com/watch?v=m13_w0h6s4M",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Blood Hound»",
+  "https://www.youtube.com/watch?v=5NRy-Xv2yQI",
+  "Get rich or die tryin´"
+);
+AgregarCancion(
+  "«Back Down»",
+  "https://www.youtube.com/watch?v=3Opytt7bblU",
+  "Get rich or die tryin´"
+);
 
-
-
-
-
-    // Cancion random + link
-    
-    cancion.innerHTML = '<i class="fas fa-compact-disc"></i> '+'<a target="_blank" id="linkCancion" href="'+ arrayCanciones[numRandom].linkyt + '">' + arrayCanciones[numRandom].cancion +  '</a>'
-    
-    
+// Cancion random + link
+let numRandom = generateRandomNumber(0, arrayCanciones.length);
+cancion.innerHTML =
+  '<i class="fas fa-play"></i> <a target="_blank" id="linkCancion" href="' +
+  arrayCanciones[numRandom].linkyt +
+  '">' +
+  arrayCanciones[numRandom].cancion +
+  "</a> <br>";
+album.append(" " + arrayCanciones[numRandom].album);
 
 // Fetch valores historicos
 
 async function getDhistorico() {
-    const bluelyticsAPI = await fetch('https://api.bluelytics.com.ar/v2/evolution.json');
-    const data = await bluelyticsAPI.json()
-    console.log(data[1].source)
-    let i = data.length -1
-    // let i = 0
-    let dblueChart = [ ] ;
-    let dblue21 = [];
-    let dOficialChart = [ ];
-    let doficial21= [];
-    let ejeX = [ ];
+  const bluelyticsAPI = await fetch(
+    "https://api.bluelytics.com.ar/v2/evolution.json"
+  );
+  window.dataHistorica = await bluelyticsAPI.json();
+  let i = dataHistorica.length - 1;
+  ejeX = [];
+  dataHistorica.forEach((element) => {
+    if (dataHistorica[i].source == "Blue") {
+      dblueHistorico.push(dataHistorica[i].value_sell);
+      ejeX.push(dataHistorica[i].date);
+    } else {
+      doficialHistorico.push(dataHistorica[i].value_sell);
+    }
+    i--;
+  });
+  var ctx = document.getElementById("myChart");
+  window.myChart = new Chart(ctx, {
+    type: "line",
+    options: {
+      maintainAspectRatio: false,
+    },
+    data: {
+      labels: ejeX,
+      datasets: [
+        {
+          data: dblueHistorico,
+          label: "Blue",
+          borderColor: "#3e95cd",
+          fill: false,
+          borderWidth: 1,
+          pointStyle: "dash",
+          pointRadius: 1,
+        },
+        {
+          data: doficialHistorico,
+          label: "Oficial",
+          borderColor: "#ffffff",
+          fill: false,
+          borderWidth: 1,
+          pointStyle: "dash",
+          pointRadius: 1,
+        },
+      ],
+    },
+  });
 
-
-        data.forEach(element => {
-            if (data[i].date.includes("2021")) {
-                if (data[i].source == 'Blue') {
-                    dblue21.push(data[i].value_sell)
-                    ejeX.push(data[i].date)
-
-                } else {
-                    doficial21.push(data[i].value_sell)
-                }
-            } else if (data[i].date.includes("2020")) {
-                if (data[i].source == 'Blue') {
-                    dblue21.push(data[i].value_sell)
-                    ejeX.push(data[i].date)
-
-                } else {
-                    doficial21.push(data[i].value_sell)
-                }
-            }
-            i--
-        });
-
-    // data.forEach(element => {
-    //     if (data[i].source == 'Oficial') {
-    //         let valorVenta = data[i].value_sell
-    //         dOficialChart.push(valorVenta)
-    //         ejeX.push(data[i].date)
-    //     } else {
-    //         let valorVenta = data[i].value_sell
-    //         dblueChart.push(valorVenta)
-    //     } 
-    //     i--
-    // });
-
-
-    var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: 'line',
-  options: {
-    maintainAspectRatio: false,
-},
-  data: {
-    labels: ejeX,
-    datasets: [
-      { 
-        data: dblue21,
-        label: "Blue", borderColor: "#3e95cd",
-        fill: false
-
-      },
-      { 
-        data: doficial21,
-        label: "Oficial",
-        borderColor: "#ffffff",
-        fill: false
-      },
-    ]
-  },
-  
-
-});
-
-
-    // const dblue = await data.blue;
-    // const dblueVenta = await dblue.value_sell;
-    // const decimaldblueVenta = (dblueVenta/2 - Math.trunc(dblueVenta/2))*100
-    // fiftyh1.innerHTML = Math.trunc(dblueVenta/2);
-    // fiftyFloat.innerHTML = '.' + decimaldblueVenta.toFixed(0)
-    // const dblueCompra = await dblue.value_buy;
-    // pcomprablue.innerHTML = 'Compra: ' + dblueCompra + '$'
-    // pventablue.innerHTML = 'Venta: ' +'<span>' +  dblueVenta + '</span>' + '$'
-
+  //   myChart.destroy();
+  return dataHistorica;
+  return myChart;
 }
 
-getDhistorico()
+getDhistorico();
 
+// Controles chart
 
+const ultMes = document.getElementById("ultMes");
+const dblueUltMes = [];
+const doficialUltMes = [];
 
+const ultAnio = document.getElementById("ultAnio");
+const dblueUltAnio = [];
+const doficialUltAnio = [];
+
+const historico = document.getElementById("historico");
+const dblueHistorico = [];
+const doficialHistorico = [];
+
+let ejeX = [];
+
+// Dibujar ultimo mes:
+
+ultMes.addEventListener("click", function ultimoMes() {
+  i = dataHistorica.length - 1;
+  ejeX = [];
+  dataHistorica.forEach((element) => {
+    if (dataHistorica[i].date.includes(anio + "-0" + mes)) {
+      if (dataHistorica[i].source == "Blue") {
+        dblueUltMes.push(dataHistorica[i].value_sell);
+        ejeX.push(dataHistorica[i].date);
+      } else {
+        doficialUltMes.push(dataHistorica[i].value_sell);
+      }
+    }
+    i--;
+  });
+  window.myChart.destroy();
+  redibujarTabla(dblueUltMes, doficialUltMes, ejeX);
+});
+
+// Dibujar ultimo Año:
+
+ultAnio.addEventListener("click", function ultimoAnio() {
+  i = dataHistorica.length - 1;
+  ejeX = [];
+  dataHistorica.forEach((element) => {
+    if (dataHistorica[i].date.includes(anio)) {
+      if (dataHistorica[i].source == "Blue") {
+        dblueUltAnio.push(dataHistorica[i].value_sell);
+        ejeX.push(dataHistorica[i].date);
+      } else {
+        doficialUltAnio.push(dataHistorica[i].value_sell);
+      }
+    }
+    i--;
+  });
+  window.myChart.destroy();
+  redibujarTabla(dblueUltAnio, doficialUltAnio, ejeX);
+});
+
+// Dibujar historico:
+historico.addEventListener("click", function historico() {
+  i = dataHistorica.length - 1;
+  ejeX = [];
+  dataHistorica.forEach((element) => {
+    if (dataHistorica[i].source == "Blue") {
+      dblueHistorico.push(dataHistorica[i].value_sell);
+      ejeX.push(dataHistorica[i].date);
+    } else {
+      doficialHistorico.push(dataHistorica[i].value_sell);
+    }
+    i--;
+  });
+  ejeX.pop();
+  window.myChart.destroy();
+  redibujarTabla(dblueHistorico, doficialHistorico, ejeX);
+});
+
+function redibujarTabla(arrayDolarBlue, arrayDolarOficial, tiempo) {
+  var ctx = document.getElementById("myChart");
+  window.myChart = new Chart(ctx, {
+    type: "line",
+    options: {
+      maintainAspectRatio: false,
+    },
+    data: {
+      labels: tiempo,
+      datasets: [
+        {
+          data: arrayDolarBlue,
+          label: "Blue",
+          borderColor: "#3e95cd",
+          fill: false,
+          borderWidth: 1,
+          pointStyle: "dash",
+          pointRadius: 1,
+        },
+        {
+          data: arrayDolarOficial,
+          label: "Oficial",
+          borderColor: "#ffffff",
+          fill: false,
+          borderWidth: 1,
+          pointStyle: "dash",
+          pointRadius: 1,
+        },
+      ],
+    },
+  });
+}
